f36f45ee4bb5c045b165ee77d331c9801be97270
Clinton J. Minton <clintonminton@gmail.com>
2024-10-11 18:55:54 -0500

    try compound bash ops on single line? fingers crossed!

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -10,11 +10,7 @@
       };
       onStart = {
         default.openFiles = [ "README.md" ];
-        docker_start = "( docker start codepath-cyb-101 &>/dev/null
-        && docker container exec -u 0:0 codepath-cyb-101
-        su -c 'chown 0:0 /usr/bin/sudo && chmod 4755 /usr/bin/sudo && chown -R codepath: ~codepath'
-        && docker container exec -it codepath-cyb-101 bash
-        ) || docker run -it --name codepath-cyb-101 --volume codepath-cyb-101:/home/codepath codepathdockerhub/cyber:cyb-101";
+        docker_start = "( docker start codepath-cyb-101 &>/dev/null && docker container exec -u 0:0 codepath-cyb-101 su -c 'chown 0:0 /usr/bin/sudo && chmod 4755 /usr/bin/sudo && chown -R codepath: ~codepath' && docker container exec -it codepath-cyb-101 bash ) || docker run -it --name codepath-cyb-101 --volume codepath-cyb-101:/home/codepath codepathdockerhub/cyber:cyb-101";
       };
     };
   };
