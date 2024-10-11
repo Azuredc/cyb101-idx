8ef2cf03d8c97dc9beb94e17c18e9d5ad0624099
Clinton J. Minton <clintonminton@gmail.com>
2024-10-11 18:02:00 -0500

    test sudo patch

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,11 +6,13 @@
   idx = {
     workspace = {
       onCreate = {
-        docker = "docker pull codepathdockerhub/cyber:cyb-101";
+        docker_pull = "docker pull codepathdockerhub/cyber:cyb-101";
       };
       onStart = {
         default.openFiles = [ "README.md" ];
-        docker = "docker start -ai codepath-cyb-101 &>/dev/null || docker run -it --name codepath-cyb-101 --volume codepath-cyb-101:/home/codepath codepathdockerhub/cyber:cyb-101";
+        docker_start = "docker start codepath-cyb-101 &>/dev/null || docker run -it --name codepath-cyb-101 --volume codepath-cyb-101:/home/codepath codepathdockerhub/cyber:cyb-101";
+        docker_patch = "docker container exec -u 0:0 codepath-cyb-101 su -c 'chown 0:0 /usr/bin/sudo && chmod 4755 /usr/bin/sudo && chown -R codepath: ~codepath'";
+        docker_shell = "docker container exec -it codepath-cyb-101 bash";
       };
     };
   };
